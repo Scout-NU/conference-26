64ee723f376114db154385c912d384c8e946ba13
Ivan Lam <ravenivan1@gmail.com>
2026-01-16 10:56:32 -0500

    Draft MobileNavbar. Still need to make more responsive changes to navbar. Need Entangle logo to complete.

---
+++ components/navbar/MobileNav.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { NavbarLinks } from "@/constants";
+import Link from "next/link";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/hamburger.svg"
+          alt="Menu"
+          width={36}
+          height={36}
+          className="sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent className="bg-[#323232] border-none p-6">
+        <Link href="/" className="mb-12 flex items-center">
+          <p>Entangle</p>
+        </Link>
+
+        <div>
+          <section className="flex flex-col gap-12">
+            {NavbarLinks.map((link) => (
+              <SheetClose
+                asChild
+                key={link.name}
+                className="text-2xl font-bold text-white"
+              >
+                <Link href={link.href}>
+                  <p>{link.name}</p>
+                </Link>
+              </SheetClose>
+            ))}
+          </section>
+
+          <SheetClose asChild className="mt-16 block">
+            <Link
+              href="https://www.eventbrite.com/e/entangle-2024-tickets-747955061117?aff=nextjs"
+              target="_blank"
+              className="uppercase text-2xl font-dmSans tracking-[-0.264px] font-bold text-center text-black px-8 py-4 rounded-lg bg-[#F3FF85]"
+            >
+              Tickets
+            </Link>
+          </SheetClose>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,20 +4,21 @@ import { NavbarLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
 
   const pathname = usePathname();
 
   return (
-    <nav className='flex justify-between items-center w-full bg-[#323232] px-[23px] py-8 '>
+    <nav className='flex justify-between items-center w-full bg-[#323232] px-5.75 py-8 '>
       <div>
         Entangle
       </div>
       <div className='flex justify-between items-center gap-16'>
         
         {NavbarLinks.map((link) => (
-          <a key={link.name} href={`/${link.href}`} className={`relative uppercase text-2xl font-dmSans tracking-[-0.264px] font-bold text-center ${pathname === `/${link.href}` ? "underline decoration-[#F3FF85] decoration-3 underline-offset-8" : ""}`}>
+          <a key={link.name} href={`/${link.href}`} className={`relative uppercase text-2xl font-dmSans tracking-[-0.264px] font-bold text-center ${pathname === `/${link.href}` ? "underline decoration-[#F3FF85] decoration-3 underline-offset-8" : ""} max-sm:hidden`}>
             {link.name}
             {/* <span className="absolute bottom-0 left-0 w-full h-1 bg-[#F3FF85]"></span> */}
           </a>
@@ -26,6 +27,8 @@ const Navbar = () => {
         <Link href="https://www.eventbrite.com/e/entangle-2024-tickets-747955061117?aff=nextjs" target="_blank" className='uppercase text-2xl font-dmSans tracking-[-0.264px] font-bold text-center text-black px-8 py-4 rounded-lg bg-[#F3FF85]'>
           Tickets
         </Link>
+
+        <MobileNav />
       </div>
     </nav>
   )
