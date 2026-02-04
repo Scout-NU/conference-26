8f66db644674d6a77f2e6de3b04aa7bd709636fd
Ivan Lam <ravenivan1@gmail.com>
2026-02-04 17:36:51 -0500

    fix: Responsive changes to speaker card and speakers page

--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -4,7 +4,7 @@ import TrackSection from "@/components/speakers/TrackSection";
 
 export default function Speakers() {
   return (
-    <div className="px-32 py-20 min-h-screen">
+    <div className="px-32 max-sm:px-8 py-20 min-h-screen">
 
       <h1 className="text-center md:text-left text-8xl font-bold text-[#fffcec] mt-32 md:mt-80 mb-64 max-sm:mb-32 max-sm:text-5xl">speakers</h1>
 
