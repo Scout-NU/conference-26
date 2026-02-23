a1fff1a140afc7b4c273c5bb59fa7d1650c110f2
zayj-chwy <zjahangir@chewy.com>
2026-02-22 22:13:57 -0500

    feat: Add cursor trail

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
+import CursorTrailGate from "@/components/CursorTrailGate";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistMono.variable} antialiased bg-[#212121]`}
       >
+        <CursorTrailGate />
         <Navbar />
         {children}
       </body>
